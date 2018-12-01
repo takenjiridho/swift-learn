73342db4aa901003ad2c2a151250914440cb4d9e
muhammad ridho <ridho@muhammads-Mac-mini.local>
2018-12-01 09:16:51 +0700

    i am poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by muhammad ridho on 12/1/18.
+//  Copyright © 2018 muhammad ridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -7,4 +7,17 @@ https://flatuicolors.com
 
 icon gen
 https://appicon.co
+
+logo design
+https://www.canva.com
+
+asset
+https://www.appbrewery.co/p/ios12-course-resources/
+```
+
+## note
+```
+1. sideloading , you can run app through iphone device
+  a. xcode version and ios version must same like (10.1 matches 12.1)
+
 ```
