d51bc4e98a5dbaeae3f88915c2cf0b59d3b1eadb
muhammad ridho <ridho@muhammads-Mac-mini.local>
2018-12-04 09:35:04 +0700

    diceeeeee

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by muhammad ridho on 12/4/18.
+//  Copyright © 2018 muhammad ridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let arrayImage = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    var randumDiceIndex1: Int = 0
+    var randumDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceeImageView1: UIImageView!
+    @IBOutlet weak var diceeImageView2: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func rollButtonPresed(_ sender: UIButton) {
+        randumDiceIndex1 = Int.random(in: 0 ... 5)
+        randumDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randumDiceIndex1)
+        
+        diceeImageView1.image = UIImage.init(named: arrayImage[randumDiceIndex1])
+        diceeImageView2.image = UIImage.init(named: arrayImage[randumDiceIndex2])
+        
+    }
+}
+
