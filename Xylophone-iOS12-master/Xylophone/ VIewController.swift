dc935d2715cb814be0b9dd879adc3e539253c85f
muhammad ridho <ridho@muhammads-Mac-mini.local>
2018-12-08 14:53:35 +0700

    xlyphone and quizz app

---
+++ Xylophone-iOS12-master/Xylophone/ VIewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController , AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
+    let noteArray = ["note1","note2","note3","note4","note5","note6","note7"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        let selectSound : String = noteArray[sender.tag-1]
+        
+        print(selectSound)
+        
+        playSound(selectSound)
+    }
+    
+    func playSound(_ selectedSound: String){
+        let soundUrl = Bundle.main.url(forResource: selectedSound, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        }
+        catch{
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+
+}
+
