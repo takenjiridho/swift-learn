4f3d24eead0e303362d81f80a640d99aeb53f1af
muhammad ridho <ridho@muhammads-Mac-mini.local>
2018-12-09 21:40:43 +0700

    layouting

---
+++ Auto Layout Calulator/Auto Layout Calulator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Auto Layout Calulator
+//
+//  Created by muhammad ridho on 12/9/18.
+//  Copyright © 2018 muhammad ridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+}
+
