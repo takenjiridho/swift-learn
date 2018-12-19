05eaaeef512a77d7371320d01c795b0caacbb434
muhammad ridho <ridho@muhammads-Mac-mini.local>
2018-12-19 09:22:03 +0700

    playground closure and error handling

--- playground/ClosuresFunc.playground/Contents.swift
+++ playground/ClosuresFunc.playground/Contents.swift
@@ -1,3 +1,79 @@
 import UIKit
 
-var str = "Hello, playground"
+func calculator(n1: Int, n2:Int ) -> Int {
+    return add(v1: n1, v2: n2)
+}
+func add(v1: Int, v2:Int ) -> Int {
+  return v1+v2
+}
+//calculator(n1: 10, n2: 20)
+func multiply(v1: Int, v2:Int ) -> Int {
+    return v1*v2
+}
+
+func calculator(n1: Int, n2:Int, operation: (Int,Int ) -> Int ) -> Int {
+    return operation(n1,n2)
+}
+
+//calculator(n1: 10, n2: 20, operation: add)
+//calculator(n1: 10, n2: 20, operation: multiply)
+
+//{ (v1: Int, v2:Int ) -> Int  in
+//    return v1*v2
+//}
+
+//move to operation
+
+//calculator(n1: 10, n2: 20, operation: { (v1: Int, v2:Int ) -> Int  in
+//    return v1*v2
+//})
+
+//1 line
+calculator(n1: 10, n2: 20, operation: { (nv1, nv2 ) in nv1*nv2 } )
+
+//user charater $
+let c = calculator(n1: 10, n2: 20, operation: { $0 * $1 } )
+//print(c)
+
+//user charater $ simply
+let cc = calculator(n1: 10, n2: 20) {$0 * $1}
+//print(cc)
+
+let array = [1,2,3,6,5]
+
+func addOne (n1 :Int) -> Int {
+    return n1+1
+}
+
+//penambahan nilai array
+//array.map(addOne)
+
+
+//penambahan nilai array simple line
+//array.map({(n1) -> Int in n1+1})
+
+
+//penambahan nilai array using $
+print(array.map({$0+1}))
+
+let array2 = [5,6,8,2,4,1]
+
+//conver to arrayString
+let newArray = array2.map{"\($0)"}
+
+print(newArray)
+
+//Closuer Expression Syntax
+//{(parameter) -> returnType in Statements }
+
+
+
+
+
+
+
+
+
+
+
+
--- playground/complationHandle.playground/Contents.swift
+++ playground/complationHandle.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
-var str = "Hello, playground"
+class Firebase {
+    func createUser(username : String , password : String , completion : (Bool, Int) -> Void){
+        let isSuccess = true
+        let userID = 1223
+        
+        completion(isSuccess,userID)
+        
+    }
+}
+
+class MyApp {
+    
+    func registrationPressed() {
+        let firebase = Firebase()
+//cara satu
+//        firebase.createUser(username: "Ridho", password: "ganteng", completion: complated)//
+        
+        
+//cara 2
+        firebase.createUser(username: "Ridho", password: "ganteng") {
+                (isSuccess, userId) in
+                print ("register Success \(isSuccess)!!" )
+                print ("userId is : \(userId)")
+        }
+        
+    }
+    
+    
+    func complated (isSuccess : Bool, userId : Int) {
+        print ("register Success \(isSuccess)!!" )
+        print ("userId is : \(userId)")
+    }
+    
+    
+    
+    
+}
+
+
+let myApp = MyApp()
+myApp.registrationPressed()
